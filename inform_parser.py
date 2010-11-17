52ddeac97f3d2877671ed99164fcab18ba6ee27d
Rob Speer <rspeer@mit.edu>
2010-11-17 15:12:41 -0500

    translating names correctly now

--- inform_parser.py
+++ inform_parser.py
@@ -26,33 +26,37 @@ W_PLURAL = Literal("plural")
 # punctuation
 TILDE = Literal("~").suppress()
 ARROW = Literal("->")
+SEMICOLON = Literal(";").suppress()
 
 new_identifier = Word(alphanums+'_')
 identifier = Word(alphanums+'_')
-adjective = Word(alphas)
-name_list = Group(OneOrMore(sglQuotedString))
-num_arrows = OneOrMore(ARROW).setParseAction(len)
+name_list = Group(OneOrMore(sglQuotedString.setParseAction(lambda p: p[0].strip("'"))))
+num_arrows = ZeroOrMore(ARROW).setParseAction(len)
 quoted_name = dblQuotedString.setParseAction(lambda p: p[0].strip('"'))
 
 def name_lister(parse):
-    for name in parse[2]:
-        idsToNames[currentID].append(name)
-    return []
+    if currentID:
+        for name in parse[2]:
+            idsToNames[currentID].append(name)
+        return []
 
 def name_assigner(parse):
-    global currentID
-    idsToNames[currentID].append(parse[2])
+    if currentID:
+        idsToNames[currentID].append(parse[2])
     return []
 
 def id_assigner(parse):
     global currentID
-    assert isinstance(currentID, str)
     currentID = parse[-1]
     return []
 
-has_line = K_HAS + adjective
+def id_forgetter(parse):
+    global currentID
+    currentID = None
+
+has_line = K_HAS + identifier
 has_line.setParseAction(lambda p: [('HasProperty', p[1], True)])
-hasnt_line = K_HAS + TILDE + adjective
+hasnt_line = K_HAS + TILDE + identifier
 hasnt_line.setParseAction(lambda p: [('HasProperty', p[1], False)])
 name_line = K_WITH + W_NAME + name_list
 name_line.setParseAction(name_lister)
@@ -68,20 +72,38 @@ class_line.setParseAction(lambda p: [('IsA', p[1], True)])
 object_def = D_OBJECT + num_arrows + new_identifier + dblQuotedString.suppress()
 class_def = D_CLASS + new_identifier
 
-defn_line = (object_def | class_def).setParseAction(id_assigner)
+defn_line = ((object_def | class_def) + stringEnd).setParseAction(id_assigner)
 prop_line = (has_line | hasnt_line | name_line | canonical_name_line
-             | found_in_line | class_line)
-inform_line = (defn_line | prop_line)
+             | found_in_line | class_line) + stringEnd
+defn_end = (SEMICOLON + stringEnd).setParseAction(id_forgetter)
+inform_line = (defn_line | prop_line | defn_end)
 
 def inform_parser(file):
-    results = []
+    assertions = []
     for line in file:
         try:
             parse_out = inform_line.parseString(line.strip())
-            new_features = [(currentID,) + feature for feature in parse_out]
-            print new_features
-            results.extend(new_features)
+            if currentID is not None:
+                new_features = [(currentID,) + feature for feature in parse_out]
+                if new_features:
+                    print new_features
+                assertions.extend(new_features)
         except ParseException:
             continue
-print class_def.parseString("Class K0_kind")
+
+    named_assertions = []
+    for assertion in assertions:
+        id, rel, target, polarity = assertion
+        names = idsToNames[id]
+        for name in names:
+            if target in idsToNames:
+                targetnames = idsToNames[target]
+            else:
+                targetnames = [target]
+            for targetname in targetnames:
+                named = (name, rel, targetname, polarity)
+                print named
+                named_assertions.append(named)
+    return named_assertions
 inform_parser(open('bronze_compiled.inf'))
+
