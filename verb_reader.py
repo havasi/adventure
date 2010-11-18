20aa787a9a698552873d669b2d0f403fc0baf693
Rob Speer <rspeer@mit.edu>
2010-11-18 18:11:22 -0500

    missed a few keywords

--- verb_reader.py
+++ verb_reader.py
@@ -7,6 +7,14 @@ def verb_reader(filename):
             parts = line.split()
             for part in parts[1:]:
                 verbs.add(part.strip("'/"))
+        if line.startswith('Constant'):
+            parts = line.split()
+            if parts[1].endswith('__WD') or parts[1].endswith('__KY'):
+                keyword = parts[-1]
+                assert keyword.startswith("'")
+                assert keyword.endswith("';")
+                verbs.add(keyword[1:-2].lower().strip('/'))
     return verbs
 
-print verb_reader('bronze_compiled.inf')
+verbs = verb_reader('bronze_compiled.inf')
+print verbs
