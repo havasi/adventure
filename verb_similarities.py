2b39ffe5fbb4534fe7f8494f8a67fd3b6134b1ef
Elizabeth L. Hawkins <elhawk@mit.edu>
2010-11-18 18:15:54 -0500

    modified verb similarities so it doesn't overload the meanings of verbs that are already used for something in game

--- verb_similarities.py
+++ verb_similarities.py
@@ -1,18 +1,28 @@
 import pickle
 from csc.nl import get_nl
 from csc import divisi2
+from similarity import make_sim
 
 en_nl = get_nl('en')
+"""
 A = divisi2.network.conceptnet_matrix('en')
 A_pre = A.normalize_all()
 U_pre, S_pre, V_pre = A_pre.svd(k=100)
 sim_pre = divisi2.reconstruct_similarity(U_pre, S_pre, post_normalize=False)
+"""
+
+sim_pre = make_sim()
+
 def output_verb_similarities(filename):
     verbs = []
     f = open(filename)
     understands = pickle.load(f)
     f.close()
 
+    
+    #verbs that have meaning in game.  Shouldn't give them other meanings.  
+    dont_overload =set(['ignite', 'restore', 'all', 'adjust', 'help', 'burn', 'show', 'dance', 'skip', 'random', 'actions', 'bring', 'kill', 'rooms', 'shake', 'go', 'polish', 'yes', 'find', 'silence', 'blow', 'bang', 'squeeze', 'untie', 'pray', 'menu', 'except', 'eat', 'about', 'shout', 'version', 'descend', 'present', 'crack', 'carve', 'sorry', 'save', 'retreat', 'kick', 'take', 'swim', 'then', 'move', 'hit', 'torture', 'get', 'rules', 'hint', 'watch', 'rush', 'strut', 'break', 'answer', 'unwrap', 'every', 'darn', 'kiss', 'tie', 'press', 'awaken', 'wipe', 'restart', 'look', 'insert', 'rotate', 'exits', 'wash', 'inventory', 'drop', 'stroll', 'l', 'remove', 'leave', 'p', 'stuff', 'each', 'x', 'grab', 'steal', 'dir', 'view', 'sew', 'shed', 'set', 'knock', 'unscrew', 'pronouns', 'showme', 'prune', 'touch', 'see', 'meta', 'holler', 'run', 'wander', 'creep', 'out', 'snuff', 'scale', 'yodel', 'wreck', 'deactivate', 'jump', 'normal', 'brief', 'melt', 'everything', 'hear', 'score', 'exit', 'superbrief', 'hiss', 'drink', 'dirs', 'shut', 'full', 'showverb', 'quit', 'rub', 'thump', 'inspect', 'deliver', 'punch', 'undo', 'credits', 'consult', 'cut', 'wear', 'uncover', 'put', 'sing', 'directions', 'throw', 'wait', 'shine', 'both', 'search', 'incinerate', 'sniff', 'shove', 'roar', 'of', 'script', 'o', 'display', 'chop', 'stride', 'objects', 'place', 'stand', 'pick', 'discard', 'climb', 'think', 'gonear', 'feed', 'info', 'depart', 'verbose', 'curses', 'feel', 'wave', 'embroider', 'sweep', 'wrap', 'walk', 'whisper', 'ask', 'activate', 'long', 'another', 'carry', 'resuscitate', 'ring', 'open', 'speak', 'showheap', 'use', 'proceed', 'injure', 'free', 'scry', 'bite', 'smash', 'revive', 'relations', 'fight', 'purloin', 'attach', 'attack', 'damn', 'tell', 'lock', 'strike', 'scope', 'hug', 'inv', 'board', 'breathe', 'murder', 'complete', 'offer', 'hum', 'acquire', 'squash', 'but', 'snatch', 'shroud', 'observe', 'wake', 'stick', 'hurl', 'hold', 'cleanse', 'disrobe', 'fullscore', 'hints', 'me', 'pull', 'myself', 'sleep', 'g', 'places', 'things', 'bother', 'tree', 'scenes', 'n', 'unlock', 'shift', 'kindle', 'extinguish', 'z', 'fetch', 'say', 'and', 'listen', 'loosen', 'tap', 'give', 'taste', 'toss', 'abstract', 'describe', 'fuck', 'ransack', 'twist', 'examine', 'buy', 'embrace', 'rap', 'close', 'return', 'turn', 'seek', 'check', 'shit', 'fill', 'again', 'sip', 'end', 'no', 'pay', 'self', 'fling', 'novice', 'cross', 'verify', 'revisit', 'read', 'nap', 'other', 'amusing', 'hop', 'notify', 'test', 'destroy', 'unlockall', 'smell', 'start', 'ascend', 'drat', 'play', 'finish', 'scrub', 'trace', 'screw', 'back', 'stop', 'dive', 'swing', 'reach', 'sneak', 'hand', 'push', 'drag', 'slice', 'nouns', 'fasten', 'dust', 'glklist', 'transcript', 'swallow', 'muffle', 'stitch', 'purchase', 'short', 'don', 'remember', 'whistle', 'i', 'light', 'clear', 'sit', 'cover', 'smack', 'greet', 'doff', 'q', 'switch', 'solve', 'oops', 'clean', 'enter', 'y', 'awake', 'yell', 'scream', 'squint'])
+ 
     
     for item in understands:
         add = False
@@ -34,7 +44,6 @@ def output_verb_similarities(filename):
     for item in verbs:
         #assume that items that end in 'ing' are a verb.  NOTE: Could cause 
         #some misidentifications, "ring" comes to mind.
-        #TODO: detect verbs + stopwords, for instance "putting it on"
 
         print item
         #to extract the content of the verb phrase, need to split it at first stopword
@@ -55,7 +64,8 @@ def output_verb_similarities(filename):
             sims = sim_pre.row_named(verb)
             for similar_item in sims.top_items():
                 new_verb = [similar_item[0] + ' ' + rest, item[1]]
-                print new_verb
+                if similar_item[0] not in dont_overload:
+                  print new_verb
         except KeyError:
             pass
         print '\n' 
