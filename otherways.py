b8b9179f68e3f242b95ffbbf40390a5a5b5707e9
Catherine Havasi <havasi@media.mit.edu>
2010-11-19 14:59:00 -0500

    working on near miss

--- otherways.py
+++ otherways.py
@@ -2,9 +2,19 @@ from csc import divisi2
 from csc.divisi2.blending import blend
 from csc.util.persist import PickleDict
 from nltk.corpus import wordnet as wn
+from verb_reader import verb_reader
 
 pd = PickleDict('./pickledir/')
 
+def make_exclude_file(game):
+    overlist = []
+    gamematix = divisi2.load(game + '.pickle')
+    nouns = gamematix.row_labels
+    overlist += nouns
+    overlist += verb_reader(game+'.inf')
+    overlist = [x.lower() for x in overlist]
+    return overlist
+    
 def getWNSim(word1, word2):
     syn1 = wn.synsets(word1)
     syn2 = wn.synsets(word2)
@@ -23,14 +33,12 @@ def NotOverRide(game, object):
     if game in pd and 'over' in pd[game]:
         overlist = pd[game]['over']
     else:
-        file = open(game + '.over', 'r')
-        overlist = file.readlines()
-        file.close()
-        overlist = [x.lower().replace('\n', '') for x in overlist]
+        overlist = make_exclude_file(game)
+        pd[game]['over'] = overlist
     if object in overlist: return False
     return True
 
-def top_game_sims(game, object, n=6):
+def top_non_game_sims(game, object, n=6):
     if game in pd:
         similarity = pd[game]['blend']
     else:
@@ -38,7 +46,7 @@ def top_game_sims(game, object, n=6):
     frame = divisi2.SparseVector.from_counts([object])
     sim = similarity.right_category(frame)
     all = sim.top_items(n*2)
-    return all
+    return [x for x in all if NotOverRide(game, x)]
 #    clear = [(getWNSim(object, x[0]), x) for x in all if NotOverRide(game, x)]
 #    clear.sort()
 #    return [x[1] for x in clear][:n]
@@ -50,18 +58,20 @@ def make_blend(thefile):
     blended_matrix = blend([conceptnet, thegame])
     u,s,v = blended_matrix.svd()
     
-    similarity = divisi2.reconstruct_similarity(u, s, offset=3) #, post_normalize=False)
+    similarity = divisi2.reconstruct_similarity(u, s, offset=3) 
     pd.mkdir(thefile.split('.')[0])
     pd[thefile.split('.')[0]]['blend'] = similarity
+    game_obj_sim = divisi2.reconstruct_similarity(u, s, offset=0.1)
+    pd[thefile.split('.')[0]]['game_sim'] = game_obj_sim
     return similarity
 
 def understand(game, object):
-    top_stuff = top_game_sims(game, object)
+    top_stuff = top_non_game_sims(game, object)
     return 'Understand "' + '" or "'.join([x[0] for x in top_stuff]) + '" as the ' + object + '.'
 
 print understand('bronze', 'hut')
-print understand('bronze', 'house')
+'''print understand('bronze', 'house')
 print understand('bronze', 'taste')
 print understand('bronze', 'cow')
 print understand('bronze', 'castle')
-print understand('bronze', 'old woman')
+print understand('bronze', 'old woman')'''
