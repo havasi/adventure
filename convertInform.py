85ee7a82fb7c23351c939465e6d1738f17dd6718
Catherine Havasi <havasi@media.mit.edu>
2011-05-02 15:29:04 -0400

    hi

--- convertInform.py
+++ convertInform.py
@@ -144,8 +144,8 @@ def pullAllTopics(filename):
 	subjectRegex = makeSubjectRegex()
 	suggestsRegex = makeSuggestsRegex()
 	topicList = getEmilyShortTopics(text, subjectRegex, suggestsRegex)
-	understandsRegexes = makeBoatloadOfUnderstandsRegexes():
-	understandDict = getEmilyShortUnderstands(text, understandRegexes):
+	understandsRegexes = makeBoatloadOfUnderstandsRegexes()
+	understandDict = getEmilyShortUnderstands(text, understandsRegexes)
 	topicList.extend(understandDict.keys())
 	return topicList
 
--- endtoend.py
+++ endtoend.py
@@ -1,12 +1,27 @@
 from convertInform import pullAllTopics
 #convertFile, pullAllTopics
 from otherways import synonyms
+import divisi2
 
 # Davis Square
 #topics = pullAllTopics('DavisSquare.txt')
 #para = synonyms('davissquare', topics, 0)
 #convertFile(para, 'DavisSquare.txt', 'AIwashere.txt')
 
+conceptnet = divisi2.network.conceptnet_matrix('en').normalize_all()
+u,s,v = conceptnet.svd()
+similarity = divisi2.reconstruct_similarity(u, s) # offset=1.5)
+
 # Glass
+def make_sim_rough(object, topics, n=2):
+    frame = divisi2.SparseVector.from_counts([object])
+    sim = similarity.right_category(frame)
+    all = sim.top_items(n*2)
+    return [x for x in all if x[0] not in topics]
+
+
 topics = pullAllTopics('Glass.txt')
-assert False
+for object in topics:
+    print object
+    print make_sim_rough(object, topics)
+
